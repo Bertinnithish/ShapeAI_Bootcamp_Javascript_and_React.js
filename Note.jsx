94a5b5b73df38ac49b7889302c2baeeafae457ca
Bertinnithish <70995806+Bertinnithish@users.noreply.github.com>
2021-06-07 05:37:26 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Javascript and React.js</h1>
+      <p>
+        Hi this is Bertin Nithish, I gained more knowledge about Javascript and
+        React.Js and this bootcamp helps me to know more about Javascript and
+        React.js. Thank you for this bootcamp
+      </p>
+    </div>
+  );
+}
+
+export default Note;
